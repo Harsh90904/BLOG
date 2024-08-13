0fd6db8ade419400464595a0ec7eb2bc7a3f8eb9
Harsh_Diyora <146904128+Harsh90904@users.noreply.github.com>
2024-08-13 09:04:24 +0530

    blogpost.js

--- models/blogpost.js
+++ models/blogpost.js
@@ -3,8 +3,9 @@ const mongoose = require('mongoose');
 const BlogPost = new mongoose.Schema({
     title: { type: String , required: true },
     content: { type: String , required: true },
-    author:{ type: mongoose.Schema.Types.ObjectId, ref: 'User' ,  required: true },
+    author:{ type: mongoose.Schema.Types.ObjectId, ref: 'User'},
     createdAt:{type:Date, default: Date.now()},
+    userId: String
 });
 const blog = mongoose.model('BlogPost', BlogPost);
 
